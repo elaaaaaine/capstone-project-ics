57c15922186ccda3a96a797196e582c15bbdd73f
elaaaaaine <elaine.chen@student.csd509j.net>
2025-06-06 12:05:03 -0700

    key added

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+    export const levle = SpriteKind.create()
+}
 function createSprite () {
     for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
         mySprite = sprites.create(img`
@@ -22,7 +26,7 @@ function createSprite () {
         tiles.setTileAt(value, assets.tile`transparency16`)
         controller.moveSprite(mySprite, 100, 0)
         scene.cameraFollowSprite(mySprite)
-        mySprite.ay = 110
+        mySprite.ay = 90
     }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -31,6 +35,121 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         numberofjumps += 1
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite, effects.smiles, 500)
+})
+function createNewLevel () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile1`)) {
+        elle = sprites.create(img`
+            ....................
+            ....cc..............
+            ..ccbbcc............
+            .cbbbbbbc...........
+            .cbbccbbc...........
+            cbbccbbbbc..........
+            cbbcbbbbbc..........
+            .cbbbbbbc...........
+            .cbbbbbbbc..........
+            ..ccbbcbbbc.........
+            ....cc.cbbbc........
+            ........cbbbc.......
+            .........cbbbc......
+            ..........cbbbc.....
+            ..........ccbbbc....
+            ........cccbbbbbc...
+            ........cbbbccbbbc..
+            ........ccbc.cbbbbc.
+            .........cc.cbbcbbbc
+            ...........cbbcccbbc
+            ...........ccbc..ccc
+            ............ccc.....
+            ....................
+            ....................
+            ....................
+            `, SpriteKind.levle)
+    }
+    tiles.placeOnTile(elle, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
+function createCoins () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile0`)) {
+        coin = sprites.create(img`
+            . . b b b b . . 
+            . b 5 5 5 5 b . 
+            b 5 d 3 3 d 5 b 
+            b 5 3 5 5 1 5 b 
+            c 5 3 5 5 1 d c 
+            c d d 1 1 d d c 
+            . f d d d d f . 
+            . . f f f f . . 
+            `, SpriteKind.coin)
+        animation.runImageAnimation(
+        coin,
+        [img`
+            . . b b b b . . 
+            . b 5 5 5 5 b . 
+            b 5 d 3 3 d 5 b 
+            b 5 3 5 5 1 5 b 
+            c 5 3 5 5 1 d c 
+            c d d 1 1 d d c 
+            . f d d d d f . 
+            . . f f f f . . 
+            `,img`
+            . . b b b . . . 
+            . b 5 5 5 b . . 
+            b 5 d 3 d 5 b . 
+            b 5 3 5 1 5 b . 
+            c 5 3 5 1 d c . 
+            c 5 d 1 d d c . 
+            . f d d d f . . 
+            . . f f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . b 5 d 1 5 b . 
+            . b 5 3 1 5 b . 
+            . c 5 3 1 d c . 
+            . c 5 1 d d c . 
+            . . f d d f . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . . b 1 1 b . . 
+            . . b 5 5 b . . 
+            . . b d d b . . 
+            . . c d d c . . 
+            . . c 3 3 c . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . b 5 1 d 5 b . 
+            . b 5 1 3 5 b . 
+            . c d 1 3 5 c . 
+            . c d d 1 5 c . 
+            . . f d d f . . 
+            . . . f f . . . 
+            `,img`
+            . . . b b b . . 
+            . . b 5 5 5 b . 
+            . b 5 d 3 d 5 b 
+            . b 5 1 5 3 5 b 
+            . c d 1 5 3 5 c 
+            . c d d 1 d 5 c 
+            . . f d d d f . 
+            . . . f f f . . 
+            `],
+        500,
+        false
+        )
+        tiles.placeOnTile(coin, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+    }
+}
+let coin: Sprite = null
+let elle: Sprite = null
 let numberofjumps = 0
 let mySprite: Sprite = null
 let jump_speed = 0
@@ -157,8 +276,9 @@ scene.setBackgroundImage(img`
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
-jump_speed = -110
+jump_speed = -80
 createSprite()
+createCoins()
 game.onUpdate(function () {
     if (mySprite.vy == 0) {
         numberofjumps = 0
